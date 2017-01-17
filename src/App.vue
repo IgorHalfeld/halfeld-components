776e1c6af37c3522c7b5020486088ea3523a1612
Igor Luíz <igor@devstacks.com.br>
2017-01-17 13:43:00 -0200

    Change props on app.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         title-big-word="Luíz"
         title-small-word="Igor"
         overlay-color="tomato"
-        image-src="http://igorluiz.me/assets/img/halfeld.jpg">
+        image-src="https://avatars.githubusercontent.com/u/9022134?v=3%22,%20%22gravatar_id%22:%20%22%22,">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </HalfeldCard>
     </HalfeldMenu>
